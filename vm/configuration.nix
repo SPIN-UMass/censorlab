41bd5150eae54fbf51e972c88e785e5cdf771b2c
Jade Harley <jade@witchof.space>
2024-10-02 19:42:56 -0400

    allow unfree

--- vm/configuration.nix
+++ vm/configuration.nix
@@ -11,7 +11,7 @@ in
   imports =
     [
     ];
-
+  nixpkgs.config.allowUnfree = true;
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
